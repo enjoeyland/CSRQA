6818c66e085ef49d50850ccd850297620eb749db
enjoeyland <khmin1104@gmail.com>
2022-10-04 00:26:10 +0900

    download preprocessed data

--- qagnn/download_preprocessed_data.sh
+++ qagnn/download_preprocessed_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mv data data_old
+# mv data data_old
 
 wget https://nlp.stanford.edu/projects/myasu/QAGNN/data_preprocessed_release.zip
 unzip data_preprocessed_release.zip
-mv data_preprocessed_release  data
+mv data_preprocessed_release  preprocessed_data
